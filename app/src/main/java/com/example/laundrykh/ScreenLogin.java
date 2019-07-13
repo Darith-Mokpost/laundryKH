0c36af4887822959c4f514bd5df4a804c759e9e2
Darith-Mokpost <darith0001@gmail.com>
2019-07-13 08:28:55 +0700

    add screen sign up

--- app/src/main/java/com/example/laundrykh/ScreenLogin.java
+++ app/src/main/java/com/example/laundrykh/ScreenLogin.java
@@ -1,31 +1,24 @@
 package com.example.laundrykh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+
 
 public class ScreenLogin extends AppCompatActivity {
+    EditText edtUsername;
+    EditText edtPassword;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_login);
-        String[] spinnerTitles;
-        String[] spinnerPopulation;
-        int[] spinnerImages;
-
-        spinnerTitles = new String[]{"+855", "+93", "+86", "France", "Germany", "India", "Ireland", "Italy", "Mexico", "Poland"};
-        spinnerPopulation = new String[]{"24.13 Million", "207.7 Million", "1.379 Billion", "66.9 Million", "82.67 Million", "1.324 Billion", "4.773 Million", "60.6 Million", "127.5 Million", "37.95 Million"};
-        spinnerImages = new int[]{
-                  R.drawable.flag_australia
-                , R.drawable.flag_brazil
-                , R.drawable.flag_china
-                , R.drawable.flag_france
-                , R.drawable.flag_germany
-                , R.drawable.flag_india
-                , R.drawable.flag_ireland
-                , R.drawable.flag_italy
-                , R.drawable.flag_maxico
-                , R.drawable.flag_poland};
     }
-
 }
