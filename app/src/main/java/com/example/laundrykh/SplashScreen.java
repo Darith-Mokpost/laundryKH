7130190d4e5b5d1021533ef436828d32f23a5d93
Darith-Mokpost <darith0001@gmail.com>
2019-07-20 18:52:25 +0700

    new config

--- app/src/main/java/com/example/laundrykh/SplashScreen.java
+++ app/src/main/java/com/example/laundrykh/SplashScreen.java
@@ -2,13 +2,26 @@ package com.example.laundrykh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN_TIME = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        if(getSupportActionBar()!=null)
+            this.getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME);
     }
 }
